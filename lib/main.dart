161b25ea442fb1beadc54762a5c811556889c326
Parth Agarwal <parth1102007@gmail.com>
2020-06-30 23:51:07 +0530

    Added skeleton

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Expense Planner'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              color: Colors.blue,
+              width: double.infinity,
+              child: Text('Chart'),
+            ),
+            Column(
+              children: <Widget>[
+                Text('Transaction1'),
+                Text('Transaction2'),
+              ],
+            )
+        ]),
+      ),
+    );
+  }
+}
