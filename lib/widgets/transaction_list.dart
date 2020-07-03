e4c00e83d327c693ce5405fbb797d0e7605901c9
Parth Agarwal <parth1102007@gmail.com>
2020-07-04 01:23:17 +0530

    added list view

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -7,44 +7,47 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2,
+    return Container(
+      height: 300,
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                  ),
+                  width: 120,
+                  child: Text(
+                    'Rs. ${tx.amt}/-',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                width: 120,
-                child: Text(
-                  'Rs. ${tx.amt}/-',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(tx.title, style: TextStyle(fontSize: 18)),
+                    Text(DateFormat.yMMMd().format(tx.date), style: TextStyle(fontSize: 12, color: Colors.blueGrey))
+                  ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(tx.title, style: TextStyle(fontSize: 18)),
-                  Text(DateFormat.yMMMd().format(tx.date), style: TextStyle(fontSize: 12, color: Colors.blueGrey))
-                ],
-              ),
-            ],
-          ),
-          elevation: 5,
-        );
-      }).toList(),
+              ],
+            ),
+            elevation: 5,
+          );
+        }).toList(),
+      ),
 
     );
   }
